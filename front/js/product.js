b410fa7572408e42ce2f7b0da4a3f097fb53f64e
0-Lili-0 <leslie.bayon@gmail.com>
2022-07-19 16:13:58 +0200

    debut page produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,1 +1,10 @@
-console.log("test");
+// recupération id d'un produit
+const getProductId = new URL(window.location.href).searchParams.get("id");
+console.log(" voici l'id du produit " + getProductId)
+
+
+
+fetch("http://localhost:3000/api/products" + getProductId)
+    .then(response => response.json())
+    .then(data => console.log(data))
+
