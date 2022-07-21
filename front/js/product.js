fdbe2dcc68848e8eb739dc00e5e428e00ca7ba03
0-Lili-0 <leslie.bayon@gmail.com>
2022-07-21 12:56:36 +0200

    Ajout des infos produits sur la page produit

--- front/js/product.js
+++ front/js/product.js
@@ -2,9 +2,64 @@
 const getProductId = new URL(window.location.href).searchParams.get("id");
 console.log(" voici l'id du produit " + getProductId)
 
+// recupérer les données d'un produits à partir de l'id
+fetch("http://localhost:3000/api/products/"+getProductId)
+    .then (response => response.json())
+    .then (product => {
+        console.log(product);
+        displayData(product)
+    });
 
+// affichage des produits
+function displayData(object) {
+    const altTxt = object.altTxt
+    const imageUrl = object.imageUrl
+    const name = object.name
+    const description = object.description
+    const price = object.price
+    const colors = object.colors
 
-fetch("http://localhost:3000/api/products" + getProductId)
-    .then(response => response.json())
-    .then(data => console.log(data))
+    createImage(imageUrl, altTxt)
+    createTitle(name)
+    createPrice(price)
+    createDescription(description)
+    createColors(colors)
+}
+// creation de l'image
+function createImage(imageUrl, altTxt) {
+    const image = document.createElement('img')
+    image.src = imageUrl
+    image.alt = altTxt
+    const img = document.querySelector(".item__img")
+    if ( img != null) img.appendChild(image)
+}
+// creation du nom produit
+function createTitle(name) {
+    const h1 = document.querySelector("#title")
+    if (h1 != null) h1.textContent = name
+}
+//cretation prix produit
+function createPrice(price) {
+    const span = document.querySelector("#price")
+    if (span != null) span.textContent = price
+}
+//creation description produit
+function createDescription(description) {
+    const p = document.querySelector("#description")
+    if (p != null) p.textContent = description
+}
+// creation couleur
+function createColors(colors) {
+    const select = document.querySelector("#colors")
+    if (select != null ) {
+        //creation boucle pour le choix des couleurs
+        colors.forEach(color => {
+            const option = document.createElement("option")
+            option.value = color
+            option.textContent = color
+            select.appendChild(option)
+        });
+    }
+}
 
+// gestion bouton ajout aux panier
