a2c5ddfe6f73f95f55f42ef3c32eb906a36d3ebb
0-Lili-0 <leslie.bayon@gmail.com>
2022-08-18 17:15:20 +0200

    page confirmation afficher orderId

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,2 +1,8 @@
+// recuperation numero commande
 const orderId = new URL(window.location.href).searchParams.get("commande");
 console.log(" voici l'id du produit " + orderId)
+
+// affichage numero commande
+const div = document.getElementById("orderId")
+
+div.textContent = orderId;
